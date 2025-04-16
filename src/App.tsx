8ed5249d594c8163fca04d757d7fc95197761d43
Owen <owen@ownwn.com>
2025-04-16 18:38:39 +1200

    Remove base CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NotFound from "./routes/notFound.tsx";
 import { HomePage } from "./routes/homePage.tsx";
