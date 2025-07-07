ebbc3cf341c716c44b14eaf6ec5a654a4a7153bc
Owen <64725743+Ownwn@users.noreply.github.com>
2025-07-07 18:03:12 +1200

    update courses

--- src/routes/home/homePage.tsx
+++ src/routes/home/homePage.tsx
@@ -2,13 +2,11 @@ import styles from "./homePage.module.css";
 import { useEffect, useState } from "react";
 
 import email from "../../assets/images/email.svg";
-import grades from "../../assets/images/grades.svg";
 import hammer from "../../assets/images/hammer.svg";
 import machine_learning from "../../assets/images/machine-learning.svg";
 import network from "../../assets/images/network.svg";
-import pc from "../../assets/images/pc.svg";
-import ticket from "../../assets/images/ticket.svg";
-import tutor from "../../assets/images/tutor.svg";
+import coding from "../../assets/images/coding.svg";
+import cybr from "../../assets/images/cybr.svg";
 
 
 export function HomePage() {
@@ -39,25 +37,25 @@ export function HomePage() {
                     <a href={links[0]}><img src={network}
                                             alt="NWEN"
                                             className={styles.fitImage}/></a>
-                    <div className={styles.imageName}>NWEN ECS</div>
+                    <div className={styles.imageName}>NWEN</div>
                 </div>
 
                 <div className={styles.imageBox}>
                     <a href={links[1]}><img
                         src={machine_learning} alt="aiml" className={styles.fitImage}/></a>
-                    <div className={styles.imageName}>AIML ECS</div>
+                    <div className={styles.imageName}>AIML</div>
                 </div>
 
                 <div className={styles.imageBox}>
                     <a href={links[2]}><img
                         src={hammer} alt="SWEN" className={styles.fitImage}/></a>
-                    <div className={styles.imageName}>SWEN ECS</div>
+                    <div className={styles.imageName}>SWEN</div>
                 </div>
 
                 <div className={styles.imageBox}>
                     <a href={links[3]}><img
-                        src={pc} alt="COMP" className={styles.fitImage}/></a>
-                    <div className={styles.imageName}>COMP ECS</div>
+                        src={cybr} alt="CYBR" className={styles.fitImage}/></a>
+                    <div className={styles.imageName}>CYBR</div>
                 </div>
 
             </div>
@@ -68,31 +66,16 @@ export function HomePage() {
 
 
             <div className={styles.imageContainer}>
-
-
                 <div className={styles.imageBox}>
-                    <a href={links[4]}><img src={tutor} alt="tutor"
-                                            className={styles.fitImage}/></a>
-                    <div className={styles.imageName}>Nuku</div>
-                </div>
-
-                <div className={styles.imageBox}>
-                    <a href={links[5]}><img src={email} alt="email"
+                    <a href={links[4]}><img src={email} alt="email"
                                             className={styles.fitImage}/></a>
                     <div className={styles.imageName}>Email</div>
                 </div>
 
                 <div className={styles.imageBox}>
-                    <a href={links[6]}><img src={ticket} alt="email"
+                    <a href={links[5]}><img src={coding} alt="wats"
                                             className={styles.fitImage}/></a>
-                    <div className={styles.imageName}>Tickets</div>
-                </div>
-
-                <div className={styles.imageBox}>
-                    <a href={links[7]}><img src={grades}
-                                            alt="grades"
-                                            className={styles.fitImage}/></a>
-                    <div className={styles.imageName}>Marking</div>
+                    <div className={styles.imageName}>WATS</div>
                 </div>
 
             </div>
