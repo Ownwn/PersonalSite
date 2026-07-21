114f31c82a2430b8ca9815997ddd462252c9b030
Owen <owen@ownwn.com>
2026-07-21 13:51:50 +1200

    update links

--- src/routes/home/homePage.tsx
+++ src/routes/home/homePage.tsx
@@ -3,6 +3,10 @@ import {useEffect, useState} from "react";
 
 import email from "../../assets/images/email.svg";
 import coding from "../../assets/images/coding.svg";
+import cybr from "../../assets/images/cybr.svg";
+import grades from "../../assets/images/grades.svg";
+import network from "../../assets/images/network.svg";
+import ticket from "../../assets/images/ticket.svg";
 
 
 export function HomePage() {
@@ -36,12 +40,42 @@ export function HomePage() {
                 </div>
 
                 <div className={styles.imageBox}>
-                    <a href={links[1]}><img src={coding} alt="garfield"
+                    <a href={links[1]}><img src={cybr} alt="cybr"
                                             className={styles.fitImage}/></a>
-                    <div className={styles.imageName}>Garfield</div>
+                    <div className={styles.imageName}>CYBR</div>
                 </div>
 
+                <div className={styles.imageBox}>
+                    <a href={links[2]}><img src={ticket} alt="ticket"
+                                            className={styles.fitImage}/></a>
+                    <div className={styles.imageName}>SPAN</div>
+                </div>
             </div>
+
+            <div className={styles.imageContainer}>
+                <div className={styles.imageBox}>
+                    <a href={links[3]}><img src={coding} alt="coding"
+                                            className={styles.fitImage}/></a>
+                    <div className={styles.imageName}>SWEN</div>
+                </div>
+
+                <div className={styles.imageBox}>
+                    <a href={links[4]}><img src={network} alt="network"
+                                            className={styles.fitImage}/></a>
+                    <div className={styles.imageName}>SWEN4</div>
+                </div>
+
+                <div className={styles.imageBox}>
+                    <a href={links[5]}><img src={grades} alt="grades"
+                                            className={styles.fitImage}/></a>
+                    <div className={styles.imageName}>MARK</div>
+                </div>
+            </div>
+
+
+
+
+
         </div>
     );
 }
